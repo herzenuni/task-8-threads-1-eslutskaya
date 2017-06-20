b21bd85698309236d71bc666ca9d303f4f95dff0
eslutskaya <eslutskaya@gmail.com>
2017-06-21 01:09:03 +0300

    Update main.py
    задание выполнено

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-def matrix_multiply(X, Y):
-    """ здесь необходимо написать доктест """
-    # Z = X * Y
-    pass
+#Выполнила Слуцкая Екатерина ИВТ, 2 курс
+import threading
 
+matrixX = [[1, 2], [3, 4]]
+matrixY = [[5, 6], [7, 8]]
+
+
+def calculate_element(row, col):
+    Z = 0
+
+    for i, item in enumerate(row):
+        Z += item * col[i]
     return Z
 
 
-if __name__ == '__main__':
-    # здесь необходимо написать код
-    # для тестирования
+def calculate_row(rowX, matrixY):
+    cols = [[row[i] for row in matrixY] for i in range(len(matrixY[0]))]
+    Z = list(map(lambda a: calculate_element(rowX, a), cols))
+    print(Z)
+    return Z
+
+
+for row in matrixX:
+    threading.Thread(target=calculate_row, args=(row, matrixY)).start()
